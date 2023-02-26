68721176ea790377f4b9f8214a40e752b4a08039
davizinho <davi.bestguy@gmail.com>
2023-02-26 01:41:23 -0500

    js not working atm

---
+++ js/progress.js
@@ -0,0 +1,12 @@
+const image_input = document.querySelector("#image_input");
+
+let uploaded_image = "";
+
+image_input.addEventListener("change", function(){
+    const reader = new FileReader();
+    reader.addEventListener("load", () => {
+        uploaded_image = reader.result;
+        document.querySelector("#upload").style.backgroundImage = 'url(${uploaded_image})';
+    });
+    reader.readAsDataURL(this.files[0]);
+})
