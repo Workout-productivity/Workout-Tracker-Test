c2e101dffc09d6b4f8a7fba14bd84f4df1c39d36
davizinho <davi.bestguy@gmail.com>
2023-02-26 10:52:04 -0500

    fixed js

--- html/js/progress.js
+++ html/js/progress.js
@@ -3,6 +3,7 @@ const image_input = document.querySelector("#image_input");
 var uploaded_image = "";
 
 image_input.addEventListener("change", function(){
+    //console.log(image_input.value);
     const reader = new FileReader();
 
     reader.addEventListener("load", () => {
