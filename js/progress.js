81b1f5d23c45c8bf5fb616475409c1d77b146eb0
davizinho <davi.bestguy@gmail.com>
2023-02-26 01:53:06 -0500

    image js

--- js/progress.js
+++ js/progress.js
@@ -1,12 +1,14 @@
 const image_input = document.querySelector("#image_input");
 
-let uploaded_image = "";
+var uploaded_image = "";
 
 image_input.addEventListener("change", function(){
     const reader = new FileReader();
+
     reader.addEventListener("load", () => {
         uploaded_image = reader.result;
-        document.querySelector("#upload").style.backgroundImage = 'url(${uploaded_image})';
+        document.querySelector("#display_image").style.backgroundImage = `url(${uploaded_image})`;
     });
+
     reader.readAsDataURL(this.files[0]);
 })
